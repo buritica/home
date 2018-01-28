448f379635fb5212276d900733dcb70f1b17b284
Juan Pablo Buritica <buritica@gmail.com>
2018-01-28 13:48:44 -0500

    [add] typography, normalize css and logo

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,33 @@
-const Index = () => (
+import Head from 'next/head'
+import normalize from 'normalize.css/normalize.css'
+
+import typography from '../utils/typography'
+import { TypographyStyle, GoogleFont } from 'react-typography'
+const { rhythm } = typography
+
+export default () => (
   <div>
-    <p>Hello Next.js</p>
+    <Head>
+      <title>🇨🇴 Colombia.dev 🇨🇴</title>
+      <meta charSet='utf-8' />
+      <TypographyStyle typography={typography} />
+      <GoogleFont typography={typography} />
+    </Head>
+
+    <div class='container'>
+      <img id='logo' src='./static/logo_condor.svg' alt='Colombia.dev Logo Condor ' />
+      <h1>colombia.dev</h1>
+      <style jsx global>{normalize}</style>
+      <style jsx>{`
+
+        .container {
+          
+        }
+
+        #logo {
+
+        }
+      `}</style>
+    </div>
   </div>
 )
-
-export default Index
